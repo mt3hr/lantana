12abf3b1084ab2cceb2d7377a6d7bda372afcbf5
mt3hr <ymt3shr@gmail.com>
2023-05-30 00:23:32 +0900

    細かい調整

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -13,6 +13,9 @@ import { createVuetify } from 'vuetify'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  display: {
+    mobileBreakpoint: 'sm',
+  },
   theme: {
     themes: {
       light: {
